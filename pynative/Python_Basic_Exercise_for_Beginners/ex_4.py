0b95c18e3764a3f6c4325bfbd9bf696cea119978
igor-tsap <tsap.ihor@gmail.com>
2025-11-06 19:19:15 +0200

    add: ex 4

---
+++ pynative/Python_Basic_Exercise_for_Beginners/ex_4.py
@@ -0,0 +1,16 @@
+# Write a Python code to remove characters from a string from 0 to n and return a new string.
+#
+# Given:
+#
+def remove_chars(word, n):
+    return word[n:]
+
+print("Removing characters from a string")
+print(remove_chars("pynative", 4))
+# output 'tive' first four characters are removed
+
+print(remove_chars("pynative", 2))
+# output 'native'
+# Use string slicing to get a substring.
+# Think about how you can use the slicing notation [:] along with the value of n
+# to select the portion of the string after the first n characters.
