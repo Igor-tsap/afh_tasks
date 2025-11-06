e730681549d3a10e4412e89f9e3148524488d287
igor-tsap <tsap.ihor@gmail.com>
2025-11-06 18:51:29 +0200

    add: ex 3

---
+++ pynative/Python_Basic_Exercise_for_Beginners/ex_3.py
@@ -0,0 +1,9 @@
+# Exercise 3: Print characters present at an even index number
+#
+# Write a Python code to accept a string from the user and display characters present at an even index number.
+#
+# For example, str = "PYnative". so your code should display ‘P’, ‘n’, ‘t’, ‘v’.
+#
+my_string = input("Enter text: ")
+for letter in range(0, len(my_string), 2):
+    print(my_string[letter])
