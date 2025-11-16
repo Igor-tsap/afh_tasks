113a4c97e1297ae59aff0a9d1d42e528c8610702
igor-tsap <tsap.ihor@gmail.com>
2025-11-16 10:54:44 +0200

    add: while loop

---
+++ chat_GPT_tasks/Python_While_Loops.py
@@ -0,0 +1,25 @@
+# Create a while loop that prints numbers from 1 to 20, but only the even ones. Use variables, numbers, and operators you already know.
+#================================Task 1 start========================================
+# number = 1
+# while number <= 20:
+#     number += 1
+#     if number % 2 == 0:
+#         print(number)
+#================================Task 1 end==========================================
+# Ask the user to type a word. Keep asking again in a while loop until the user types the word "stop". When the loop ends, print "Finished".
+#================================Task 2 start========================================
+# word = input("Enter a word: ")
+# while word != "stop":
+#     word = input("Enter a word: ")
+# else:
+#     print("Finished")
+#================================Task 2 end==========================================
+# Create a list of several fruits. Use a while loop to print each fruit one by one. Stop the loop early if the fruit is "banana".
+# Use lists, conditions, and booleans.
+#================================Task 3 start========================================
+# fruits = ["apple", "orange", "pear", "mango", "banana", "apricot", "pineapple"]
+# number = 0
+# while  fruits[number] != "banana":
+#     print(fruits[number])
+#     number += 1
+#================================Task 3 end==========================================
