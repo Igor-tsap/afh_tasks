044d8ff426e6971a614b6154a70973089b1f5132
igor-tsap <tsap.ihor@gmail.com>
2026-01-07 19:02:10 +0200

    update flask2

--- projects/flask2/app.py
+++ projects/flask2/app.py
@@ -1,23 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, flash
+from werkzeug.utils import redirect
+
 
 app = Flask(__name__)
+app.secret_key = "456"
 
-@app.route("/", methods = ["GET", "POST"])
-def index():
-    my_list = list(range(1, 11))
-    my_dict = {
-        "dreamlike":"Dale",
-        "uncanny":"Laura",
-        "ominous":"Betty",
-        "disquieting":"Rita",
-        "surreal":"Henry",
-        "hypnotic":"Jeffrey",
-        "shadowed":"Frank",
-        "eerie":"Dorothy",
-        "fragmented":"Alvin",
-        "subconscious":"Gordon"
+my_list = list(range(1, 11))
+my_dict = {
+    "dreamlike": "Dale",
+    "uncanny": "Laura",
+    "ominous": "Betty",
+    "disquieting": "Rita",
+    "surreal": "Henry",
+    "hypnotic": "Jeffrey",
+    "shadowed": "Frank",
+    "eerie": "Dorothy",
+    "fragmented": "Alvin",
+    "subconscious": "Gordon"
 }
-    return render_template("index.html", my_list=my_list, my_dict=my_dict)
+
+
+@app.route("/", methods = [ "POST", "GET",])
+def index():
+    if request.method == "POST":
+        new_adj = request.form.get("adj")
+        new_name = request.form.get("name")
+
+        new_row = {new_adj: new_name}
+
+        my_dict.update(new_row)
+
+        flash("row was successfully added")
+        return redirect(url_for("index"))
+
+    else:
+        return render_template("index.html", my_list=my_list, my_dict=my_dict)
 
 
 
