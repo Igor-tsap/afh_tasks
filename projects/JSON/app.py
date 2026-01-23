27c94a4b446356096f9a69d147681d97f65781d2
igor-tsap <tsap.ihor@gmail.com>
2026-01-23 10:57:30 +0200

    update project json

--- projects/JSON/app.py
+++ projects/JSON/app.py
@@ -1,121 +1,17 @@
-from flask import Flask, redirect, render_template, request, url_for, flash
-import mysql.connector
-
-
-config = {
-    'user': 'root',
-    'password': 'root',
-    'host': 'localhost',
-    "port": 8889,
-    'database': '1stdb',
-    'raise_on_warnings': True,
-}
-
-db = mysql.connector.connect(**config)
-cursor = db.cursor()
+from flask import Flask, jsonify
+import requests
 
 app = Flask(__name__)
-app.secret_key = "68464"
 
+URL = "https://6972638632c6bacb12c6c80b.mockapi.io/animals"
 
-
-@app.route("/", methods= ["GET", "POST"])
+@app.route("/")
 def index():
-    if request.method == "POST":
-        sql = "INSERT INTO animals (name, price, quantity) VALUES (%s, %s, %s)"
-        val = (
-            request.form.get("name"),
-            request.form.get("price"),
-            request.form.get("quantity")
-        )
+    response = requests.get(URL)
+    data = response.json()
+    
+    return jsonify(data)
 
-        cursor.execute(sql, val)
-
-        flash("data was successfully added")
-        return redirect(url_for("index"))
-
-    else:
-        sort =  request.args.get("sort", "id")
-        order = request.args.get("order", "asc")
-
-        allowed_columns = {
-            "name": "name",
-            "price": "price",
-            "quantity": "quantity",
-            "id": "id"
-        }
-
-        allowed_order = {
-            "asc": "ASC",
-            "desc": "DESC"
-        }
-
-        sort_col = allowed_columns.get(sort, "id")
-        sort_order = allowed_order.get(order, "ASC")
-
-        sql = f"SELECT * FROM animals ORDER BY {sort_col} {sort_order}"
-
-        cursor.execute(sql)
-        data = cursor.fetchall()
-
-
-        return render_template("index.html", data=data, sort=sort, order=order)
-
-@app.route("/delete/<int:id>")
-def delete(id):
-    sql = "DELETE FROM animals WHERE id = %s"
-    val = (id,)
-
-    cursor.execute(sql, val)
-
-    db.commit()
-    flash(f"row {id} was successfully deleted")
-    return redirect("/")
-
-@app.route("/update/<int:id>", methods = ["GET", "POST"])
-def update(id):
-    if request.method == "POST":
-        sql = "UPDATE animals SET name = %s, price = %s, quantity = %s WHERE id = %s"
-        val = (
-               request.form.get("name"),
-               request.form.get("price"),
-               request.form.get("quantity"),
-               id,
-               )
-
-        cursor.execute(sql, val)
-
-        db.commit()
-        flash(f"row {id} was successfully updated")
-
-        return redirect("/")
-
-    else:
-        sql = "SELECT * FROM animals WHERE id = %s"
-        val = (id,)
-
-        cursor.execute(sql, val)
-
-        row = cursor.fetchone()
-        return render_template("update.html", row=row)
-
-@app.route("/1")
-def data1():
-    cursor.execute("SELECT * FROM animals WHERE quantity=1")
-    data = cursor.fetchall()
-    return render_template("data.html", data=data, quantity=1)
-
-@app.route("/2")
-def data2():
-    cursor.execute("SELECT * FROM animals WHERE quantity=2")
-    data = cursor.fetchall()
-    return render_template("data.html", data=data, quantity=2)
-
-@app.route("/3+")
-def data3():
-    cursor.execute("SELECT * FROM animals WHERE quantity>=3")
-    data = cursor.fetchall()
-    return render_template("data.html", data=data, quantity="3 or more")
 
 if __name__ == "__main__":
     app.run(debug=True)
