1fa53271ee670526730142993ab8aedb5ac5733e
igor-tsap <tsap.ihor@gmail.com>
2026-01-24 10:43:34 +0200

    update json proj

--- projects/JSON/app.py
+++ projects/JSON/app.py
@@ -5,43 +5,37 @@ app = Flask(__name__)
 
 URL = "https://6972638632c6bacb12c6c80b.mockapi.io/animals"
 
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "GET":
         response = requests.get(URL)
-        data = response.json()
-        return jsonify(data), response.status_code
+        return jsonify(response.json()), response.status_code
 
     if request.method == "POST":
-        u_input = request.json
+        u_input = request.get_json()
         response = requests.post(URL, json=u_input)
-        data = response.json()
-        return jsonify(data), response.status_code
+        return jsonify(response.json()), response.status_code
 
 @app.route("/posts/<int:id>", methods=["GET", "PUT", "PATCH", "DELETE"])
 def the_object(id):
     if request.method == "GET":
         response = requests.get(f"{URL}/{id}")
-        data = response.json()
-        return jsonify(data), response.status_code
+        return jsonify(response.json()), response.status_code
 
     if request.method == "PUT":
-        u_input = request.json
+        u_input = request.get_json()
         response = requests.put(f"{URL}/{id}", json=u_input)
-        data = response.json()
-        return jsonify(data), response.status_code
+        return jsonify(response.json()), response.status_code
 
     if request.method == "PATCH":
-        u_input = request.json
+        u_input = request.get_json()
         response = requests.patch(f"{URL}/{id}", json=u_input)
-        data = response.json()
-        return jsonify(data), response.status_code
+        return jsonify(response.json()), response.status_code
 
     if request.method == "DELETE":
         response = requests.delete(f"{URL}/{id}")
-        data = {"deleted": True}
-        return jsonify(data), response.status_code
-
+        return jsonify({"deleted": True}), response.status_code
 
 
 if __name__ == "__main__":
